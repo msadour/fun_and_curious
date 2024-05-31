65f9526d0e59718d0138fcb52196fe38f7e80087
Mehdi <sadour.mehdi@gmail.com>
2024-06-01 01:07:35 +0200

    Remove unused logic.

--- app/endpoints/game/utils.py
+++ app/endpoints/game/utils.py
@@ -5,18 +5,17 @@ import pdfkit
 from django.db.models.query import QuerySet
 from django.shortcuts import render
 
-from app.endpoints.game.models import Game, QuestionCategoryGame
+from app.endpoints.game.models import Game
 from app.endpoints.question.models import Category, Question
 from app.endpoints.question.serializers import QuestionSerializer
 from app.endpoints.user_profile.models import Profile
 
 
 def generate_game(label: str, author: Optional[Profile] = None) -> list:
-
     category_ids: list = [
         cat.id for cat in Category.objects.all() if cat.question_set.count() > 3
     ]
-    random_category_ids: list = random.sample(category_ids, 4)
+    random_category_ids: list = random.sample(category_ids, 6)
     randoms_categories: QuerySet[Category] = Category.objects.filter(
         id__in=random_category_ids
     )
@@ -26,33 +25,18 @@ def generate_game(label: str, author: Optional[Profile] = None) -> list:
         new_game.author = author
         new_game.save()
 
-    questions: list = []
+    questions = []
     for category in randoms_categories:
-        question_ids: list = []
-        for question in Question.objects.filter(category=category):
-            question_ids.append(question.id)
-            QuestionCategoryGame.objects.create(
-                question=question,
-                category=category,
-                game=new_game,
-            )
-
-        question_ids: list = [
+        question_ids = [
             question.id for question in Question.objects.filter(category=category)
         ]
-        length_question_ids: int = len(question_ids)
-
-        if length_question_ids > 10:
-            random_count = random.randint(3, 7)
-            random_question_ids: list = random.sample(question_ids, random_count)
-        else:
-            random_count = random.randint(1, length_question_ids)
-            random_question_ids: list = random.sample(question_ids, random_count)
-
+        random_count = random.randint(5, 8)
+        random_question_ids: list = random.sample(question_ids, random_count)
         randoms_questions: QuestionSerializer = QuestionSerializer(
             data=Question.objects.filter(id__in=random_question_ids), many=True
         )
         randoms_questions.is_valid()
+
         questions.append(
             {"category": category.label, "questions": randoms_questions.data}
         )

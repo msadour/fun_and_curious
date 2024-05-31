8f59a43f9ee7bd0f1d2c487b7c8ca2f25a70346c
Mehdi <sadour.mehdi@gmail.com>
2024-06-01 00:30:37 +0200

    Add more fixtures. Delete unused.

--- app/endpoints/game/management/commands/init_data.py
+++ app/endpoints/game/management/commands/init_data.py
@@ -1,6 +1,7 @@
 from django.core.management import BaseCommand, call_command
 
 from app.endpoints.question.models import Category, Question
+from app.endpoints.user_profile.models import Profile
 
 
 class Command(BaseCommand):
@@ -9,6 +10,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         Question.objects.all().delete()
         Category.objects.all().delete()
+        Profile.objects.all().delete()
 
         call_command("loaddata", "./fixtures/category.json")
         call_command("loaddata", "./fixtures/question_animals.json")
@@ -16,10 +18,8 @@ class Command(BaseCommand):
         call_command("loaddata", "./fixtures/question_clothes_women.json")
         call_command("loaddata", "./fixtures/question_gastronomy.json")
         call_command("loaddata", "./fixtures/question_hobbies.json")
-        call_command("loaddata", "./fixtures/question_love.json")
         call_command("loaddata", "./fixtures/question_object.json")
         call_command("loaddata", "./fixtures/question_personality.json")
-        call_command("loaddata", "./fixtures/question_polemic.json")
         call_command("loaddata", "./fixtures/question_sport.json")
         call_command("loaddata", "./fixtures/question_travel.json")
         call_command("loaddata", "./fixtures/users.json")

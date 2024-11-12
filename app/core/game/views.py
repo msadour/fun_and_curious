1e94e69de08737fb43d7becc21c148c14aca1a81
Mehdi <sadour.mehdi@gmail.com>
2024-11-12 11:27:21 +0100

    Handle formats response (JSON).

--- app/core/game/views.py
+++ app/core/game/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from app.core.game.serializers import GameSerializer
 from app.layer.utils import build_response, create_pdf
@@ -13,6 +14,7 @@ class RandomQuestionsViewSet(viewsets.ViewSet):
         gender: str = request.data.get("gender")
         only_soft: bool = request.data.get("only_soft")
         language: str = request.data.get("language")
+        format_game: str = request.data.get("format_game")
 
         game_created = self.serializer_class().create(
             validated_data={
@@ -25,6 +27,9 @@ class RandomQuestionsViewSet(viewsets.ViewSet):
         )
 
         game_data = GameSerializer(game_created).data
+        if format_game == "JSON":
+            return Response(data=game_data)
+
         file_name = game_created.file_name
         create_pdf(
             request=request,

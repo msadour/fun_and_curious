85779c1089b403f0dcb5113d4ee7ac292d68535a
Mehdi <sadour.mehdi@gmail.com>
2024-11-23 00:47:32 +0100

    Put back db tests settings.

--- fun_and_curious/settings/local.py
+++ fun_and_curious/settings/local.py
@@ -5,11 +5,11 @@ DEBUG = True
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get("DB_HOST"),
-        "PORT": os.environ.get("DB_PORT"),
+        "NAME": os.environ.get("DB_NAME", "test_db"),
+        "USER": os.environ.get("DB_USER", "test_user"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "test_password"),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
         "TEST": {
             "NAME": "fun_and_curious",
         },

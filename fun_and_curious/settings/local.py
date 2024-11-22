3c6d2f0e81fe244d0289e3c68292ab2c562c5c1b
Mehdi <sadour.mehdi@gmail.com>
2024-11-22 22:47:36 +0100

    Add test name for db.

--- fun_and_curious/settings/local.py
+++ fun_and_curious/settings/local.py
@@ -10,6 +10,9 @@ DATABASES = {
         "PASSWORD": os.environ.get("DB_PASSWORD"),
         "HOST": os.environ.get("DB_HOST"),
         "PORT": os.environ.get("DB_PORT"),
+        "TEST": {
+            "NAME": "fun_and_curious",
+        },
     }
 }
 
